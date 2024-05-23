ab6a2ff8c360106dd396a131d0c8976c33934c22
aliintaa <atkzn.05@mail.ru>
2024-05-23 15:40:17 +0300

    все страницы

--- Pet4You/Pages/AuthPage.xaml.cs
+++ Pet4You/Pages/AuthPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,36 @@ namespace Pet4You.Pages
         public AuthPage()
         {
             InitializeComponent();
+        }
+        int code = 0;
+        private void SendCodeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MailAddress toAddress = new MailAddress(EmailTb.Text);
+            MailAddress fromAddress = new MailAddress("burtovdanil2005@yandex.ru");
+            MailMessage message = new MailMessage(fromAddress, toAddress);
+            Random random = new Random();
+            code = random.Next(1000, 10000);
+            message.Body = code.ToString();
+            message.Subject = "no:reply|Код для входа в приложение";
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.yandex.ru";
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("burtovdanil2005@yandex.ru", "apfrqmsqkawgbikb");
+            smtpClient.Send(message);
+            EnterCode.Visibility = Visibility.Collapsed;
+            ApplyCode.Visibility = Visibility.Visible;
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            if (CodeTb.Text == code.ToString())
+            {
+                MessageBox.Show("Успешный вход!");
+            }
         }
     }
 }
